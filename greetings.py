d641775c5bcfa026d8ff471b8e879cd15c10741c
Khavaev <50018500+Khavaev@users.noreply.github.com>
2025-01-27 21:01:21 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Paul'
+title = 'NewNote1'
+content = 'Some content'
+status = 'New'
+created_date = '27-01-2025'
+issue_date = '31-12-2025'
+print("Имя пользователя: ", username)
+print("Заголовок заметки: ", title)
+print("Описание заметки: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", created_date)
+print("Дата истечения заметки (дедлайн): ", issue_date)
